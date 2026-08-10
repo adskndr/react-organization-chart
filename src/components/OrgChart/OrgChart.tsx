d09f72164b4d0c7599e3061f41cd7390f7efd0d7
adskndr <adis.s98@hotmail.com>
2026-08-10 10:34:57 +0200

    yoo

--- src/components/OrgChart/OrgChart.tsx
+++ src/components/OrgChart/OrgChart.tsx
@@ -399,6 +399,7 @@ export const OrgChart: React.FunctionComponent<IOrgChartProps> = (
           });
           return;
         }
+        if (cancelled || !startFromUserId) return;
         const profileResponse = await getUserProfile(
           sp,
           startFromUserId,
